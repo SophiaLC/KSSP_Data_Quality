38252a2f9e69c1beaf0b6ea752e2b6da00d26a94
Sophia Crossen <30636396+SophiaLC@users.noreply.github.com>
2018-09-13 14:29:50 -0500

    Update pct55pct_Run.R

--- pct55pct_Run.R
+++ pct55pct_Run.R
@@ -1,5 +1,5 @@
 
-ch <- odbcConnect("BioSense_Platform", "BIOSENSE\\scrossenks", "fuu9QQIOH")
+ch <- odbcConnect("BioSense_Platform", "BIOSENSE\\lskywalker01", "xwingred5")
 pct <- pct55pct(channel = ch, month = 07, year = 2017)
 
 odbcCloseAll()
